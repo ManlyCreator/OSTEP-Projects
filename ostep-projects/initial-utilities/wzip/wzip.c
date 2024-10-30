d1e68ee2b9ef04ded73df50459b0a21b252eaf17
Rami <myersrami17@gmail.com>
2024-10-30 18:26:53 -0500

    Changed to fread and fwrite, still need to figure out tester errors

--- ostep-projects/initial-utilities/wzip/wzip.c
+++ ostep-projects/initial-utilities/wzip/wzip.c
@@ -12,22 +12,24 @@ int main(int argc, char** argv) {
   }
   for (int i = 1; i < argc; i++) {
     FILE* file = fopen(argv[i], "rb");
-    char line[BUFSIZE];
-    while (fgets(line, BUFSIZE, file)) {
-      char zippedLine[BUFSIZE];
-      char lastChar; 
-      int charCounter = 1;
-      for (int j = 1; j < strlen(line); j++) {
-        lastChar = line[j - 1]; 
-        char word[BUFSIZE];
-        if (lastChar == line[j]) {
-          charCounter++;
-        } else {
-          sprintf(word, "%d%c", charCounter, line[j]);
-          strcat(zippedLine, word);
-        }
+    if (!file) {
+      printf("wzip: error opening file\n");
+    }
+    char character[2];
+    char lastCharacter[2] = "";
+    int characterCounter = 1;
+    char zippedLine[BUFSIZE] = "";
+    char word[BUFSIZE];
+    while (fread(character, 1, 1, file)) {
+      if (strcmp(lastCharacter, "") != 0 && strcmp(character, lastCharacter) == 0) {
+        characterCounter++;
+      } else {
+        sprintf(word, "%d", characterCounter);
+        fwrite(word, 1, 1, stdout);
+        fwrite(character, 1, 1, stdout);
+        characterCounter = 1;
       }
-      fwrite(zippedLine, strlen(line), 1, stdout);
+      strcpy(lastCharacter, character);
     }
   }
   return 0;
