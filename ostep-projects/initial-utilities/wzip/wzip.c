0b8c7627d275b94e455e32ed21c39c50d796fa56
Rami <myersrami17@gmail.com>
2024-10-30 16:31:34 -0500

    Almost finished wzip

--- ostep-projects/initial-utilities/wzip/wzip.c
+++ ostep-projects/initial-utilities/wzip/wzip.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-// TODO: Write to stdout using the specified algorithm
-// in binary format
+// TODO: Debug current algorithm
+
+#define BUFSIZE 256
 
 int main(int argc, char** argv) {
   if (!argv[1]) {
@@ -11,9 +12,23 @@ int main(int argc, char** argv) {
   }
   for (int i = 1; i < argc; i++) {
     FILE* file = fopen(argv[i], "rb");
-    char line[256];
-    fgets(line, 256, file);
-    fwrite(line, strlen(line), 1, stdout);
+    char line[BUFSIZE];
+    while (fgets(line, BUFSIZE, file)) {
+      char zippedLine[BUFSIZE];
+      char lastChar; 
+      int charCounter = 1;
+      for (int j = 1; j < strlen(line); j++) {
+        lastChar = line[j - 1]; 
+        char word[BUFSIZE];
+        if (lastChar == line[j]) {
+          charCounter++;
+        } else {
+          sprintf(word, "%d%c", charCounter, line[j]);
+          strcat(zippedLine, word);
+        }
+      }
+      fwrite(zippedLine, strlen(line), 1, stdout);
+    }
   }
   return 0;
 }
