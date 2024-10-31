c7e5e1695813d82144bfe79f768c8d7080c0da29
Rami <myersrami17@gmail.com>
2024-10-31 08:56:27 -0500

    Fixed binary formatting and passed all tests

--- ostep-projects/initial-utilities/wzip/wzip.c
+++ ostep-projects/initial-utilities/wzip/wzip.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 #define BUFSIZE 256
 
@@ -8,41 +7,36 @@ int main(int argc, char** argv) {
     printf("wzip: file1 [file2 ...]\n");
     return 1;
   }
+  char currentChar;
+  char lastChar;
+  int charOccurrences = 1;
+  int ctr = 0;
   for (int i = 1; i < argc; i++) {
     FILE* file = fopen(argv[i], "rb");
     if (!file) {
       printf("wzip: error opening file\n");
       return 1;
     }
-    char character[2];
-    char lastChar[2] = "";
-    int charOccurrences = 1;
-    char occurrencesBuffer[BUFSIZE];
-    int ctr = 0;
-    while (fread(character, 1, 1, file)) {
+    while (fread(&currentChar, 1, 1, file)) {
       // If the last character and character are equal,
       // increment the occurrences
-      if (strcmp(lastChar, character) == 0) {
+      if (currentChar == lastChar) {
         charOccurrences++;
       // Otherwise, print the last character and its number
       // of occurrences as long as ctr > 0 
       // (ensures that the first character is properly counted)
       } else if (ctr > 0) {
-        // occurrences is printed to a string buffer so 
-        // it can be printed through fwrite
-        sprintf(occurrencesBuffer, "%d", charOccurrences); 
-        fwrite(occurrencesBuffer, 4, 1, stdout);
-        fwrite(lastChar, 1, 1, stdout);
+        fwrite(&charOccurrences, 4, 1, stdout);
+        fwrite(&lastChar, 1, 1, stdout);
         charOccurrences = 1;
       }
-      strcpy(lastChar, character);
+      lastChar = currentChar;
       ctr++;
     }
-    // The last character will never be printed in the loop,
-    // so it is printed afterwards
-    sprintf(occurrencesBuffer, "%d", charOccurrences); 
-    fwrite(occurrencesBuffer, 4, 1, stdout);
-    fwrite(lastChar, 1, 1, stdout);
   }
+  // The last character will never be printed in the loop,
+  // so it is printed afterwards
+  fwrite(&charOccurrences, 4, 1, stdout);
+  fwrite(&lastChar, 1, 1, stdout);
   return 0;
 }
